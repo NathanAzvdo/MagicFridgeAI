54aba3408b4c0d48951fd6d59674344d34a09d29
NathanAzvdo <nathan.azevedo28@gmail.com>
2025-04-20 10:36:33 -0300

    feat: post and delete routes

--- src/main/java/dev/Java10xCourse/MagicFridgeAI/controller/FoodItemController.java
+++ src/main/java/dev/Java10xCourse/MagicFridgeAI/controller/FoodItemController.java
@@ -5,10 +5,7 @@ import dev.Java10xCourse.MagicFridgeAI.service.FoodItemService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import dev.Java10xCourse.MagicFridgeAI.controller.DTO.FoodItemDTO;
 
 import java.util.List;
@@ -44,6 +41,34 @@ public class FoodItemController {
             FoodItemDTO foodItemDTO = new FoodItemDTO(foodItem.getId(), foodItem.getName(), foodItem.getCategory(), foodItem.getQuantity(), foodItem.getValidity());
             return ResponseEntity.status(HttpStatus.OK).body(foodItemDTO);
         }).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build()));
+    }
+
+    @PostMapping()
+    public ResponseEntity<?> saveFoodItem(@RequestBody FoodItemDTO foodItemDTO){
+        try {
+            FoodItem foodItem = new FoodItem(foodItemDTO.id(), foodItemDTO.name(), foodItemDTO.category(), foodItemDTO.quantity(), foodItemDTO.validity());
+            FoodItem savedFoodItem = service.save(foodItem);
+            FoodItemDTO savedFoodItemDTO = new FoodItemDTO(savedFoodItem.getId(), savedFoodItem.getName(), savedFoodItem.getCategory(), savedFoodItem.getQuantity(), savedFoodItem.getValidity());
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedFoodItemDTO);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao salvar!");
+        }
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<?> deleteFoodItem(@RequestParam Long id){
+        try {
+            Optional<FoodItem> item = service.findById(id);
+            if (item.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Item não encontrado!");
+            }
+            else{
+                service.deleteById(id);
+                return ResponseEntity.status(HttpStatus.OK).body("Item deletado com sucesso!");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao deletar!");
+        }
     }
 
 }
